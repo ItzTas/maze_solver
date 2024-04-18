ac9f2451b4550ac0c8dc7eb3363328953e193df9
ItzTas <talessabini2@gmail.com>
2024-04-18 13:52:59 -0300

    I created the break_entrance_and_exit and the _break_walls_r methods for the maze class

--- src/cell.py
+++ src/cell.py
@@ -1,7 +1,7 @@
 from graphics import Line, Point
 
 class Cell():
-    def __init__(self, win=None) -> None:        
+    def __init__(self, win=None, visited=False) -> None:        
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -11,24 +11,41 @@ class Cell():
         self._y1 = None
         self._y2 = None
         self._win = win
+        self.visited = False
 
     def draw(self, x1, y1, x2, y2) -> None:
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
+        white_fill = "white"
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, white_fill)
+            
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, white_fill)
+            
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, white_fill)
+            
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, white_fill)
             
     def draw_move(self, to_cell, undo=False) -> None:
         fill_color = "gray"
@@ -41,3 +58,4 @@ class Cell():
             fill_color = "red"
         connection = Line(Point(center_x, center_y), Point(center_x_other, center_y_other))
         self._win.draw_line(connection, fill_color)
+    
--- src/main.py
+++ src/main.py
@@ -3,17 +3,17 @@ from cell import Cell
 from maze import Maze
 
 def main():
-    num_rows = 12
+    num_rows = 18
     num_cols = 16
-    margin = 50
-    screen_x = 800
-    screen_y = 600
-    cell_size_x = (screen_x - 2 * margin) / num_cols
-    cell_size_y = (screen_y - 2 * margin) / num_rows
+    margin1 = 10
+    margin2 = 10
+    screen_x = 890
+    screen_y = 690
+    cell_size_x = (screen_x - 2 * margin1) / num_cols
+    cell_size_y = (screen_y - 2 * margin2) / num_rows
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
-    maze._create_cells()
+    maze = Maze(margin1, margin2, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 
